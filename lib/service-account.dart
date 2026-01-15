c6d9b0b87ce6a98aa3f166aa8cad0ebd5506feda
abibahAag <abibah.aag@gmail.com>
2026-01-16 03:16:24 +0700

    memperbaiki error notif & profilee

---
+++ lib/service-account.dart
@@ -0,0 +1,16 @@
+// buat folder: assets/private/service-account.json
+
+
+// {
+//   "type": "service_account",
+//   "project_id": "aag4u-5878f",
+//   "private_key_id": "7aa0be3ad53250f7828a58c49c76b5d10358bcfc",
+//   "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC0tDEV+6y9xBqx\ny6T06Wx24817M/yDI4iU0PlQ7DWOvmIuwbxyGNoH8bcobcN9Z/92sfF+cd1WrhRO\n8rXznLp1gUoz5Q3a3RRjlW/gsm1VJSJ7hBhF6cSTNhGmHfHgAvPiep6vEj9ZRy2J\nAlW7XbxTXUjthsGm6SsqvLH/cf0nZ2to6nqxOwlNM1PoflN1tJEs6lXszXT/Ab4n\nA4q6YFXcvqfMYNaBlg3iNOeIKENr1GAAYmmvSCrS51RXIHXfBSVnHmt736VXSTdk\nZ5Px165tb80jXs0hSYV1mbSHkIaT3NlseNoIl7v62Yitpmtf5d6y9G03iR+r8mmf\n48hSIX/PAgMBAAECggEABdYyJbB2NmpF+yCFnfwyKtmV0q5tsBCu7IZJdJyEkwKW\n6kVgtbC+ziCNFssLfu/5LOzMzOrsSCH2+aErpl2Vb46IW9/ApaVGxNFYBpfMzJdr\n+IsI5lfe0msd1nyYykWzefPAT2L//9K656DXLbuhMHIkFQ6pZPTwFivNILVJBNjr\nfmE2lzK6cPSYyL/k15BaW5TpPL9VpYn5BArK+3u6zalHiSp1evozao3OEG3j0baX\niGLwWjbGnRnFw/zD5czqnEcw2yLkh6GbGxhdPX9jVjhRTBBaACpuXCX0/ZaXvzaJ\nKuO/Qtx29ApOLlFpp5yWn7IRafJ2wmL58t4/Yr68cQKBgQDxyS9boWhSH3oQ7bGp\nhX48RVjoSzpoS0h6zeSQH8mRMA3mbhPBkDDkWr3g3lkihRKExawCQ+Pk/N68sM8O\nz52Z+fT9TnSjLZmQ3daPW84zJm9YUQrCIus7wOZaeBkuwfY+fLt6pUvt0Kd2N7KY\nmpSkZ8isaeo56/3CfPm+5xCAPwKBgQC/U70FAs3cfC+btFAcBpoc4QudcSGQiXE9\nTLAUBwCC/+ydWzzwfRgJ2oVSisZKVFXfJGdDYeclTJqyzv0lCquPt1wLt3Su765k\nbilLodU3mdgP1bJN45G4/0JceUEfm8SoMBuac6Koy5KcvEPYc6gSNaQxB4XwjSJi\n6Ebx5a4ccQKBgFTm5YdSjVRRE0cAgGNbqfE2FLs1N2RKE0B0Li9hyiD+c9MXkj5l\nCJYXOxfMXBZgDhmrY/RDrLHLDdfKalr0Eo5wFFQiiZiHOaaCD4a1oRmbBHxJsPJs\n2pdX7EuWYpllqLdWi2rMwnXW0AcWFdO6bDmy6+eHJaWDNc9mnTgo4UV3AoGAczCW\nT8sFaCa4fxULWiQ7awGu/YZcP+oP7GkcZaJlk3lD2uVQhjW6YExZEn4MrjmiTatc\nJHxwLsu1I8xtKn0HF4q8dy1nqd8R7tNIN4nilgwgwj/VjwFg/bFpIcW7CK2+s8fr\nBo1yNBAEYCsU6d+QJA55oS83W8GNH2FbPU4cCnECgYEAySWWBFQI9Hb2OEVHxcpi\nF9SPHNcXAsoC3Rt4zArG7sYpe8uarxRyI2n9N2xXfI0JbgS4wA8WVOilKYGaa3CB\nGe7khBfeI8NGNw1+Ba8gp6jQ0i8LUbFslUVZDpXfzhAi5CUWs3e8YHf82azQeLqm\nAyY/awK8vgBeovCkUTgJ7tA=\n-----END PRIVATE KEY-----\n",
+//   "client_email": "com-flutter-aag4u@aag4u-5878f.iam.gserviceaccount.com",
+//   "client_id": "104893823078632095542",
+//   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+//   "token_uri": "https://oauth2.googleapis.com/token",
+//   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+//   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/com-flutter-aag4u%40aag4u-5878f.iam.gserviceaccount.com",
+//   "universe_domain": "googleapis.com"
+// }
